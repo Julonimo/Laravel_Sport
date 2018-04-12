f6502541c469c9218128e8e4d5bf68a46a4e1432
Johan <johan.trommenschlager@coding-academy.fr>
2018-04-12 18:28:48 +0200

    DB rdy to migrate

---
+++ Laravel_Sport/database/migrations/2018_04_12_154116_create__match_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMatchTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Matches', function (Blueprint $table) {
+            $table->increments('Match_id');
+            $table->integer('Team1_Id');
+            $table->integer('Team2_Id');
+            $table->integer('Team1_Goals');
+            $table->integer('Team2_Goals');
+            $table->string('Match_Winner');
+            $table->string('Match_Looser');
+            $table->integer('Match_Nb_Faults');
+            $table->string('Match_Meteo');
+            $table->string('Placement');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_match');
+    }
+}
